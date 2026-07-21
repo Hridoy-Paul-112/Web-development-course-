3fbb5a0f85a680aec1a60a681b5f3af558b78472
Hridoy <hridoy89hp@gmail.com>
2026-07-22 00:04:06 +0600

    first

---
+++ Getting_Started/practice_problem.js
@@ -0,0 +1,7 @@
+console.log("Hridoy Chandra Paul");
+console.log("Department of Computer Science");
+console.log("Metropolitan University");
+
+console.log("JavaScript is fun to learn!");
+
+console.log("Hello, World!");
